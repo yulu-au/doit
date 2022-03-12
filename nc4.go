c754c3488fb23f064aeee8aaa33c103f22a97357
yu <yu>
2022-03-12 23:31:19 +0800

    commit

---
+++ nc4.go
@@ -0,0 +1,25 @@
+package main
+
+/*
+判断给定的链表中是否有环。如果有环则返回true，否则返回false。
+*/
+
+func hasCycle(head *ListNode) bool {
+	if head == nil {
+		return false
+	}
+	a, b := head, head
+
+	for a != nil && b != nil {
+		a = a.Next
+		b = b.Next
+		if b != nil {
+			b = b.Next
+		}
+
+		if a == b && a != nil {
+			return true
+		}
+	}
+	return false
+}
