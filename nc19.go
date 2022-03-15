ecbaf3ac090b1c66573cfa2abea6fa11b0a9f1ce
yu <yu>
2022-03-15 22:14:02 +0800

    commit

--- nc19.go
+++ nc19.go
@@ -5,11 +5,13 @@ package main
  子数组最小长度为1。求所有子数组的和的最大值
 */
 /*
-定义dp[i]表示前i个元素(must have ith node)的连续子数组的最大和
+定义dp[i]表示前i个元素的连续子数组的最大和
 
 
 dp[i] = max(dp[i-1]+array[i],array[i])
 */
+
+//注意:所有子数组和的最大值是max(dp[1],dp[2],dp[3],dp[4].......dp[n])
 
 func FindGreatestSumOfSubArray(array []int) int {
 	// write code here
@@ -21,6 +23,7 @@ func FindGreatestSumOfSubArray(array []int) int {
 	array = array[1:]
 	for _, v := range array {
 		dp = max(v+dp, v)
+		//ret是最大的dp[i]
 		ret = max(dp, ret)
 	}
 	return ret
