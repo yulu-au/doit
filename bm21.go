8599094343915174980ab20a4ce4166259ce872f
yu <yu>
2022-03-17 14:03:37 +0800

    commit

--- bm21.go
+++ bm21.go
@@ -11,14 +11,21 @@ func minNumberInRotateArray(rotateArray []int) int {
 	if len(rotateArray) == 0 {
 		return -1
 	}
-
-	l, r := -1, len(rotateArray)
+	/*
+		根据红蓝区间模板,以及题意,端点其实都一定是在区间里
+	*/
+	l, r := 0, len(rotateArray)-1
 	for l+1 != r {
 		m := l + (r-l)>>1
-		if rotateArray[m] >= rotateArray[0] && rotateArray[m] != rotateArray[len(rotateArray)-1] {
+		//说明m一定在左区间
+		if rotateArray[m] > rotateArray[r] {
 			l = m
-		} else {
+			//说明m一定在右区间
+		} else if rotateArray[m] < rotateArray[r] {
 			r = m
+			//不确定m在哪里,不过右区间断点可以左移一步
+		} else {
+			r--
 		}
 	}
 	return rotateArray[r]
